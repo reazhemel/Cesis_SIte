d02321149272dfde740ac04bdaaa8d3e8cf9f21f
Hemel <47193108+reazhemel@users.noreply.github.com>
2022-03-20 12:04:01 +0600

    Add files via upload

---
+++ CESIS/js/script.js
@@ -0,0 +1,8 @@
+// $(window).scroll(function(){
+//   if($(this).scrollTop() > 100){
+//       $('.navbar').addClass('sticky')
+//   } else{
+//       $('.navbar').removeClass('sticky')
+//   }
+// });
+
